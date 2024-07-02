1d524a34017882898fca1863d82a364530518305
tsenanayake <Timothy.Senanayake@gamma.co.uk>
2024-07-02 15:52:03 +0100

    Added basic canvas

---
+++ src/Canvas.js
@@ -0,0 +1,22 @@
+import React, {useRef, useEffect} from 'react';
+
+const Canvas = () => {
+    const canvasRef = useRef(null);
+
+    useEffect(() => {
+        const canvas = canvasRef.current;
+        const context = canvas.getContext('2d');
+
+        //set canvas size
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+
+        context.fillStyle = 'red';
+        context.fillRect(400, 25, 1000, 1000);
+
+    }, []);
+
+    return <canvas ref={canvasRef} />;
+};
+
+export default Canvas;
